343d239f195def5b2234bf425e314ab07de61a93
Felipe Nuti <felipenuti@Felipes-MacBook-Pro.local>
2023-04-15 19:52:30 +0900

    Added function for sending request

--- contracts/Web322Client.sol
+++ contracts/Web322Client.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.9;
 
 import "./Web322.sol";
-import "@web322worker/Web322.sol"
+import "@web322worker/Web322.sol";
 
 abstract contract Web322Client {
     using Web322 for Web322.Request;
 
-    address private constant oracle_addr = "0x878474bafd53758d75a2364b82c4f82532"; 
+    address payable private constant oracle_addr = "0x878474bafd53758d75a2364b82c4f82532"; 
+
+    event Web2Request(Web322.Request request);
 
     function buildWeb322Request(
         bytes32 requestId,
@@ -20,11 +22,10 @@ abstract contract Web322Client {
     }
 
     function sendWeb322Request(
-        Web322.Request memory req
-    ) public returns (bytes32 requestId) {
-        bytes memory encodedRequest = abi.encode(req);
-
+        Web322.Request memory req,
+        uint256 amount,
+    ) public returns (bytes32 requestId) external payable {
+        (bool success, ) = oracle_addr.call{value: amount}(req);
+        require(success, "Call failed");
     }
-
-    function _rawRequest
 }
